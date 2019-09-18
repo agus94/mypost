f899b98b112d5d1baf14834d7ba87db2e1c75909
agus94 <agusismail94@gmail.com>
2019-09-18 15:59:29 +0700

    Copy lg data yg lama

---
+++ application/views/user/user_data.php
@@ -0,0 +1,63 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+    Users
+    <small>Pengguna</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+    <li class="active">Users</li>
+  </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+  <div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title">Data Users</h3>
+        <div class="pull-right">
+            <a href="<?= site_url('user/add'); ?>" class="btn btn-primary btn-flat">
+               <i class="fa fa-user-plus">Create</i>
+            </a>
+        </div>
+    </div>
+    <div class="box-body table-responsive">
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>Adress</th>
+                    <th>Level</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                $no = 1;
+                foreach ($row->result() as $key => $data) { ?>
+                <tr>
+                    <td><?= $no++; ?>.</td>
+                    <td><?= $data->username; ?></td>
+                    <td><?= $data->name; ?></td>
+                    <td><?= $data->address; ?></td>
+                    <td><?= $data->level == 1 ? "Admin" : "Kasir"; ?></td>
+                    <td>
+                        <a href="<?= site_url('user/edit'); ?>" class="btn btn-primary btn-xs">
+                            <i class="fa fa-pencil">Update</i>
+                        </a>
+                        <a href="<?= site_url('user/hapus'); ?>" class="btn btn-danger btn-xs">
+                            <i class="fa fa-trash">Delete</i>
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+  </div>
+
+</section>
+<!-- /.content -->
