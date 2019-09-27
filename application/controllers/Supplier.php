7cc663b70488505f03934240e6419ecda1ebda78
agus94 <agusismail94@gmail.com>
2019-09-27 15:47:39 +0700

    Tampil dan hapus data supplier dg CI

--- application/controllers/Supplier.php
+++ application/controllers/Supplier.php
@@ -11,11 +11,13 @@ class Supplier extends CI_Controller {
         $this->load->model('supplier_m');
     }
 
+
 	public function index()
 	{
 		$data['row'] = $this->supplier_m->get();
 		$this->template->load('template', 'supplier/supplier_data', $data);
 	}
+	
 
 	public function del($id)
 	{
---
+++ application/models/Supplier_m.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Supplier_m extends CI_Model {
+
+    // function get
+    public function get($id = null)
+    {
+        $this->db->from('supplier');
+        if ($id != null) {
+            $this->db->where('supplier_id', $id);
+        }
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function del($id)
+    {
+        $this->db->where('supplier_id', $id);
+        $this->db->delete('supplier');
+    }
+
+
+
+}
