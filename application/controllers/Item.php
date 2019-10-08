9ddca6bd84c9dd14e21be84117a80e34232cd152
agus94 <agusismail94@gmail.com>
2019-10-08 14:48:41 +0700

    Edit data dg combobox selected dr table lain

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -39,7 +39,7 @@ class item extends CI_Controller {
 			'page' => 'add',
             'row' => $item,
             'category' => $query_category,
-            'unit' => $unit,'selectedunit' => null,
+            'unit' => $unit,'selectedunit' => null
 		);
 		$this->template->load('template', 'product/item/item_form', $data);
 	}
@@ -50,11 +50,21 @@ class item extends CI_Controller {
 		$query = $this->item_m->get($id);
 		if($query->num_rows() > 0) {
 			$item = $query->row();
-			$data = array(
-				'page' => 'edit',
-				'row' => $item
-			);
-			$this->template->load('template', 'product/item/item_form', $data);
+			$query_category = $this->category_m->get();
+
+            $query_unit = $this->unit_m->get();
+            $unit[null] = '- Pilih -';
+            foreach ($query_unit->result() as $unt) {
+                $unit[$unt->unit_id] = $unt->name;
+            }
+
+            $data = array(
+                'page' => 'edit',
+                'row' => $item,
+                'category' => $query_category,
+                'unit' => $unit,'selectedunit' => $item->unit_id,
+            );
+            $this->template->load('template', 'product/item/item_form', $data);
 		} else {
 			echo "<script>alert('Data tidak ditemukan');</script>";
 			echo "<script> window.location='".site_url('item')."';</script>";
