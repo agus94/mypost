239ba6e65c3b800f8a0d5cba6655926784518f88
agus94 <agusismail94@gmail.com>
2019-11-19 16:22:08 +0700

    tampil table history stock-in dan percantik tamplan 10:51

--- application/models/Stock_m.php
+++ application/models/Stock_m.php
@@ -3,6 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Stock_m extends CI_Model {
 
+    public function get_stock_in()
+    {
+        $this->db->select('*');
+        $this->db->from('t_stock');
+        $this->db->join('p_item', 't_stock.item_id = p_item.item_id');
+        $this->db->join('supplier', 't_stock.supplier_id = supplier.supplier_id');
+        $this->db->where('type', 'in');
+        $this->db->order_by('stock_id', 'desc');
+        $query = $this->db->get();
+
+        return $query;
+    }
+
     public function add_stock_in($post)
     {
         $params = [
