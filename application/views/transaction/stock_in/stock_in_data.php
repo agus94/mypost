2e01335a8bc705824b946700af07ed9ad33c465d
agus94 <agusismail94@gmail.com>
2020-01-06 13:05:02 +0700

    Bonus membuat detail data dg popup/modal bootsrap

--- application/views/transaction/stock_in/stock_in_data.php
+++ application/views/transaction/stock_in/stock_in_data.php
@@ -48,9 +48,10 @@
                     <td class="text-right"><?= $data->qty; ?></td>
                     <td class="text-center"><?= indo_date($data->date); ?></td>
                     <td class="text-center" width="160px">
-                        <a  class="btn btn-default btn-xs" data-toggle="modal" data-target="#modal-detail" 
+                        <a id="set_dtl" class="btn btn-default btn-xs" data-toggle="modal" data-target="#modal-detail"
                         data-barcode="<?= $data->barcode; ?>"
                         data-itemname="<?= $data->item_name; ?>"
+                        data-detail = "<?= $data->detail ?>"
                         data-suppliername="<?= $data->supplier_name; ?>"
                         data-qty="<?= $data->qty; ?>"
                         data-date="<?= indo_date($data->date); ?>"
@@ -67,6 +68,68 @@
         </table>
     </div>
   </div>
-
 </section>
-<!-- /.content -->
+
+<div class="modal fade" id="modal-detail">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">Stock In Detail</h4>
+            </div>
+            <div class="modal-body table-responsive">
+                <table class="table table-bordered no-margin">
+                    <tbody>
+                        <tr>
+                            <th style="width:35%">Barcode</th>
+                            <td><span id="barcode"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Item Name</th>
+                            <td><span id="item_name"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Detail</th>
+                            <td><span id="detail"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Supplier Name</th>
+                            <td><span id="supplier_name"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Qty</th>
+                            <td><span id="qty"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Date</th>
+                            <td><span id="date"></span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+$(document).ready(function() {
+    $(document).on('click', '#set_dtl', function() {
+        var barcode = $(this).data('barcode');
+        var itemname = $(this).data('itemname');
+        var detail = $(this).data('detail');
+        var suppliername = $(this).data('suppliername');
+        var qty = $(this).data('qty');
+        var date = $(this).data('date');
+        $('#barcode').text(barcode);
+        $('#item_name').text(itemname);
+        $('#detail').text(detail)
+        $('#supplier_name').text(suppliername);
+        $('#qty').text(qty);
+        $('#date').text(date);
+        $('#detail').text('detail');
+    });
+})
+</script>
